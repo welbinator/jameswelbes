5fc5efdcdf59fe0a3c50d21f78f2b3f9be070b84
James Welbes <james.welbes@gmail.com>
2020-12-21 11:02:47 -0600

    add tinymice api

--- admin/admin_footer.php
+++ admin/admin_footer.php
@@ -1,14 +1,21 @@
  </div>
-    <!-- /#wrapper -->
+ <!-- /#wrapper -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+ <!-- jQuery -->
+ <script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+ <!-- Bootstrap Core JavaScript -->
+ <script src="js/bootstrap.min.js"></script>
 
-<script src="https://cdn.ckeditor.com/ckeditor5/12.0.0/classic/ckeditor.js"></script>
-<script src="js/script.js"></script>
-</body>
+ <script src="https://cdn.ckeditor.com/ckeditor5/12.0.0/classic/ckeditor.js"></script>
+ <script>
+     tinymce.init({
+         selector: 'textarea',
+         plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+         toolbar_mode: 'floating',
+     });
+ </script>
+ <script src="js/script.js"></script>
+ </body>
 
-</html>
+ </html>
--- admin/admin_header.php
+++ admin/admin_header.php
@@ -1,4 +1,4 @@
-<?php ob_start();?>
+<?php ob_start(); ?>
 <?php include "../admin/db.php"; ?>
 <?php include "functions.php"; ?>
 
@@ -8,7 +8,7 @@
 
 <head>
 
-    <head>
+  <head>
     <title>Shutter &mdash; Colorlib Website Template</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -21,8 +21,7 @@
 
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
-  <script>tinymce.init({ selector:'.textarea-editor' });</script>
+    <script src="https://cdn.tiny.cloud/1/7oc6yxpf300lo0d20iaoihqvz41lla6ceg6igx6r6vu6h3vj/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
   </head>
 
 </head>
