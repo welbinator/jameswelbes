8e206487251a6ac491e33d3dacb3e09b54abc259
James Welbes <james.welbes@gmail.com>
2024-09-13 23:03:38 -0500

    check php

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,8 @@
 <?php
 ob_start(); // Start output buffering
 
-ini_set('log_errors', 1);
-ini_set('error_log', '../admin/php-error.log'); // Use the path to your log file
+// ini_set('log_errors', 1);
+// ini_set('error_log', '../admin/php-error.log'); 
 ini_set('display_errors', 1); // You can turn this off in production
 error_reporting(E_ALL);
 
@@ -11,6 +11,7 @@ echo $undefined_variable;
 
 // Database connection
 require_once "../admin/db.php";
+phpinfo(); 
 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
