da864b3e37fc9d67a43a3897eed04d192ffe8e5f
James Welbes <james.welbes@gmail.com>
2024-10-07 18:20:53 -0500

    logout

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+ini_set('session.save_path', '/tmp'); // Or a custom writable directory
+session_start(); // Start the session
+
 session_destroy(); // Destroy all session data
 header('Location: login.php'); // Redirect to login page
 exit();
