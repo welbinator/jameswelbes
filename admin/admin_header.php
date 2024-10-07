3a840020d2f1b35024e8ee5abcca93723da6c4b8
James Welbes <james.welbes@gmail.com>
2024-10-07 15:50:50 -0500

    remove extra session start

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -19,7 +19,7 @@ ob_start();
 
 
 // Check if the user is logged in
-session_start();
+
 // if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 //     header('Location: login.php');
 //     exit();
