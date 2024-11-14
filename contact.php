590b47f6c68b00d1ee9ad6cae962340de2abc0ed
James <james.welbes@gmail.com>
2024-11-14 10:39:09 -0600

    add keys to captcha

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ require_once "includes/header.php";
 
 // Create a sessions folder in your project root, e.g., '/app/sessions'
 if (!file_exists(__DIR__ . '/sessions')) {
-  mkdir(__DIR__ . '/sessions', 0777, true);
+    mkdir(__DIR__ . '/sessions', 0777, true);
 }
 
 // Set the session save path
@@ -12,7 +12,6 @@ session_save_path(__DIR__ . '/sessions');
 
 // Start session for CSRF protection
 session_start();
-
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -27,86 +26,62 @@ if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
-
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
     
-  // Verify reCAPTCHA response
-  $recaptchaSecret = 'YOUR_SECRET_KEY';
-  $recaptchaResponse = $_POST['g-recaptcha-response'];
-  $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecret&response=$recaptchaResponse");
-  $responseKeys = json_decode($response, true);
+    // Verify reCAPTCHA response
+    $recaptchaSecret = '6Lf81H4qAAAAAJsN2iWDLCBqGkhYQs5blPK0t0Tq';  // Replace with your actual secret key
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecret&response=$recaptchaResponse");
+    $responseKeys = json_decode($response, true);
 
-  if (intval($responseKeys["success"]) !== 1) {
-    echo "Please complete the CAPTCHA.";
-} else {
-      // Sanitize inputs
-      $post_subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-      $post_body = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-      $post_from = filter_var($_POST['from'], FILTER_SANITIZE_EMAIL);
-      $post_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    if (!$responseKeys['success']) {
+        echo "Please complete the CAPTCHA.";
+    } else {
+        // Sanitize inputs
+        $post_subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $post_body = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+        $post_from = filter_var($_POST['from'], FILTER_SANITIZE_EMAIL);
+        $post_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
 
-      // Validate inputs
-      if (filter_var($post_from, FILTER_VALIDATE_EMAIL) && !empty($post_name) && !empty($post_subject) && !empty($post_body)) {
-          try {
-              // Setup PHPMailer
-              $mail = new PHPMailer(true);
-              // $mail->SMTPDebug = 2;
-              $mail->SMTPOptions = [
-                'ssl' => [
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true,
-                ],
-            ];
-            
-              $mail->isSMTP();
-              $mail->SMTPAuth = true;
-              $mail->SMTPSecure = 'tls';
-              $mail->Host = 'smtp-relay.brevo.com';
-              $mail->Port = 587;
-              $mail->isHTML(true);
-              $mail->Username = 'james.welbes@gmail.com';
-              $mail->Password = 'fnYJbNwtv3E5MFZS';
-              $mail->setFrom('james@apexbranding.design');
+        // Validate inputs
+        if (filter_var($post_from, FILTER_VALIDATE_EMAIL) && !empty($post_name) && !empty($post_subject) && !empty($post_body)) {
+            try {
+                // Setup PHPMailer
+                $mail = new PHPMailer(true);
+                $mail->SMTPOptions = [
+                    'ssl' => [
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true,
+                    ],
+                ];
+                
+                $mail->isSMTP();
+                $mail->SMTPAuth = true;
+                $mail->SMTPSecure = 'tls';
+                $mail->Host = 'smtp-relay.brevo.com';
+                $mail->Port = 587;
+                $mail->isHTML(true);
+                $mail->Username = 'james.welbes@gmail.com';
+                $mail->Password = 'fnYJbNwtv3E5MFZS';
+                $mail->setFrom('james@apexbranding.design');
 
-              $mail->Subject = $post_subject;
-              $mail->Body = "<h3>From: $post_name</h3><br><p>Email: $post_from</p><br>$post_body";
-              $mail->AddAddress('james.welbes@gmail.com');
+                $mail->Subject = $post_subject;
+                $mail->Body = "<h3>From: $post_name</h3><br><p>Email: $post_from</p><br>$post_body";
+                $mail->AddAddress('james.welbes@gmail.com');
 
-              // Send the email
-              $mail->send();
-
-         
-
-              //Auto-reply email
-              // $reply = new PHPMailer(true);
-              // $reply->isSMTP();
-              // $reply->SMTPAuth = true;
-              // $reply->SMTPSecure = 'ssl'; // or 'tls' based on your setup
-              // $reply->Host = getenv('MAIL_HOST'); // Using Brevo's SMTP host
-              // $reply->Port = 465; // or 587 for 'tls'
-              // $reply->isHTML(true);
-              // $reply->Username = getenv('MAIL_USERNAME');
-              // $reply->Password = getenv('MAIL_PASSWORD');
-              // $reply->setFrom(getenv('MAIL_FROM'), getenv('MAIL_FROM_NAME'));
-              // $reply->Subject = 'Thank you for reaching out!';
-              // $reply->Body = '<p>Thank you for reaching out! I will get back to you shortly.</p>';
-              // $reply->addAddress($post_from); // User's email address from the form
-
-              // Send the reply email
-              // $reply->send();
-
-          } catch (Exception $e) {
-              // Handle exceptions
-              echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-          }
-      } else {
-          echo "Invalid form submission. Please make sure all fields are filled out correctly.";
-      }
-  } 
+                // Send the email
+                $mail->send();
+            } catch (Exception $e) {
+                // Handle exceptions
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        } else {
+            echo "Invalid form submission. Please make sure all fields are filled out correctly.";
+        }
+    }
 }
-
 ?>
 
 <div class="container-fluid contact">
@@ -122,37 +97,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['csrf_token']) && hash
                 <!-- Hidden CSRF token -->
                 <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
 
-                <div class="row form-group">
-                  <div class="col-md-6 mb-3 mb-md-0">
-                    <label class="text-white" for="fname">First Name</label>
-                    <input type="text" name="name" id="fname" class="form-control" required>
-                  </div>
-                  <div class="col-md-6">
-                    <label class="text-white" for="lname">Last Name</label>
-                    <input type="text" name="lname" id="lname" class="form-control">
-                  </div>
-                </div>
+                <!-- Your other form fields here -->
 
-                <div class="row form-group">
-                  <div class="col-md-12">
-                    <label class="text-white" for="email">Email</label>
-                    <input type="email" name="from" id="email" class="form-control" required>
-                  </div>
-                </div>
-
-                <div class="row form-group">
-                  <div class="col-md-12">
-                    <label class="text-white" for="subject">Subject</label>
-                    <input type="text" name="subject" id="subject" class="form-control">
-                  </div>
-                </div>
-
-                <div class="row form-group mb-5">
-                  <div class="col-md-12">
-                    <label class="text-white" for="message">Message</label>
-                    <textarea name="message" id="message" cols="30" rows="7" class="form-control" required placeholder="Write your notes or questions here..."></textarea>
-                  </div>
-                </div>
+                <div class="g-recaptcha" data-sitekey="6Lf81H4qAAAAAEmx1r8QXaSxDEXiEXqlZIkwZsN8"></div> <!-- reCAPTCHA widget -->
 
                 <div class="row form-group">
                   <div class="col-md-12">
