cdf30d08c93fcf13dc058dd6cb582a2550673173
James <james.welbes@gmail.com>
2025-05-05 11:09:18 -0500

    add blog back

--- readyForProd.php
+++ readyForProd.php
@@ -30,7 +30,7 @@ $searchReplacePairs = [
     '<a href="bio.php">' => '<a href="bio">',
     '<a href="contact.php">' => '<a href="contact">',
     '<a href="webdesign.php">' => '<a href="webdesign">',
-    // '<a href="blog.php">' => '<a href="blog">',
+    '<a href="blog.php">' => '<a href="blog">',
     '<a href="resume.php">' => '<a href="resume">',
 ];
 
