57f40526f8693517a9eabeacafb9f98474378b42
James <james.welbes@gmail.com>
2025-05-08 10:32:13 -0500

    fix pathes

--- includes/header-styles.php
+++ includes/header-styles.php
@@ -1,12 +1,12 @@
 <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700,900" rel="stylesheet">
 
-<link rel="stylesheet" href="css/bootstrap.min-save.css">
-<link rel="stylesheet" href="css/jquery-ui.min.css">
-<link rel="stylesheet" href="css/owl.carousel.min.css">
-<link rel="stylesheet" href="css/owl.theme.default.min.css">
-<link rel="stylesheet" href="css/aos.css">
-<link rel="stylesheet" href="css/fancybox.min.css">
-<link rel="stylesheet" href="css/animista.css">
-<link rel="stylesheet" href="css/keyframes.css">
-<link rel="stylesheet" href="fonts/icomoon/style.min.css">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="/css/bootstrap.min-save.css">
+<link rel="stylesheet" href="/css/jquery-ui.min.css">
+<link rel="stylesheet" href="/css/owl.carousel.min.css">
+<link rel="stylesheet" href="/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="/css/aos.css">
+<link rel="stylesheet" href="/css/fancybox.min.css">
+<link rel="stylesheet" href="/css/animista.css">
+<link rel="stylesheet" href="/css/keyframes.css">
+<link rel="stylesheet" href="/fonts/icomoon/style.min.css">
+<link rel="stylesheet" href="/css/style.css">
